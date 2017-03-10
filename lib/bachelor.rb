c8ce852091850b5cbacf758421712e09e69ef413
David Mieloch <davidmieloch@gmail.com>
2017-03-10 01:58:41 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,37 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each {|contestant_val|
+    return contestant_val["status"] == "Winner" ? contestant_val["name"].split.shift : nil
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.map {|season, contestants|
+    contestants.map {|contestant_hash|
+      return contestant_hash["name"] if contestant_hash["occupation"] == occupation
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.map {|season, contestants|
+    contestants.map {|contestant|
+      count += 1 if contestant["hometown"]== hometown
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.map {|season, contestants|
+    contestants.map {|contestant|
+      return contestant["occupation"] if contestant["hometown"] === hometown
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = Array.new()
+  data[season].each { |contestant| ages << contestant["age"].to_i }
+  return (ages.reduce(:+).to_f / ages.length.to_f).round # http://apidock.com/ruby/Enumerable/reduce
 end
